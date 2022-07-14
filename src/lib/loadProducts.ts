cb180cdbdc17c00082ee1a2b68ccf9aaabca9cb3
Davi <davioliveira-dev@outlook.com>
2022-07-14 11:58:41 -0300

    refactor: updating load products logic

--- src/lib/loadProducts.ts
+++ src/lib/loadProducts.ts
@@ -1,7 +1,11 @@
+import fs from "fs";
+import path from "path";
 import { Product } from "../types";
-import { fetcher } from "../utils/fetcher";
 
 export async function loadProducts() {
-  const products = await fetcher<Product[]>("/api/products");
-  return products;
+  const jsonDirectory = path.join(process.cwd(), "json");
+  const fileContents = fs.readFileSync(jsonDirectory + "/data.json");
+  const data = JSON.parse(fileContents.toString()) as Product[];
+
+  return data;
 }
