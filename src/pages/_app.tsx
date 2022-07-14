f9e8a3eb63b6f3c54e3c7a8f9eb3a895ee9361df
Davi <davioliveira-dev@outlook.com>
2022-07-14 14:08:12 -0300

    feat: add custom styles by theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,17 @@
-import { useAtomValue } from "jotai";
-import { ThemeProvider } from "next-themes";
+import { ThemeProvider, useTheme } from "next-themes";
 import type { AppProps } from "next/app";
-import store from "../store";
 import { globalStyles } from "../themes";
 import { darkTheme } from "../themes/dark";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const currentTheme = useAtomValue(store.theme);
+  const { theme } = useTheme();
+
   globalStyles();
 
   return (
     <ThemeProvider
       attribute="class"
-      defaultTheme={currentTheme}
+      defaultTheme={theme}
       value={{
         light: "light",
         dark: darkTheme.className,
